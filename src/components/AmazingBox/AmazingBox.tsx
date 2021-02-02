673fc0f30a2b9f2af658b027cfd2693adf4e9808
Eivind Mjelde <eivindmjelde@gmail.com>
2021-02-02 12:26:30 +0100

    feat(amazing box): new prop

--- src/components/AmazingBox/AmazingBox.tsx
+++ src/components/AmazingBox/AmazingBox.tsx
@@ -1,13 +1,17 @@
 import React, { ReactElement } from "react";
 import styled from "styled-components";
 
-interface Props {}
+type AmazingBoxProps = {
+  madProp: string;
+};
 
 const Box = styled.div`
   padding: 100px;
   border: 2px solid green;
 `;
 
-export function AmazingBox(): ReactElement {
-  return <Box>Imma box yo!</Box>;
+export function AmazingBox({
+  madProp = "crazy box",
+}: AmazingBoxProps): ReactElement {
+  return <Box>Imma {madProp} yo!</Box>;
 }
